2ec8b8f0584503cf40b4ec340d904ef82bf0ade8
DevlTz <kauazin3339@gmail.com>
2025-09-22 17:40:26 -0300

    fix(devltz/include): ajeitei a parte que ele não estava conseguindo ler corretamente.

--- include/expr_tree.h
+++ include/expr_tree.h
@@ -1,11 +1,13 @@
 #pragma once
+
 #include <string>
+#include <vector>
 #include <memory>
 
 struct Node {
     bool is_op;
-    char op; // valid if is_op
-    long long val; // valid if !is_op
+    char op; // valid if is_op == true
+    long long val; // valid is_op == false
     Node* left;
     Node* right;
     Node(long long v): is_op(false), op('\0'), val(v), left(nullptr), right(nullptr) {}
@@ -16,3 +18,4 @@ Node* parse_rpn_expression(const std::vector<std::string>& tokens);
 long long evaluate_tree(const Node* root);
 void free_tree(Node* root);
 std::string pretty_print(const Node* root);
+
