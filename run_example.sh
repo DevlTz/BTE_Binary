f219cefbb1c74895722f10ea0e175ec6a10c542e
Kauã do Valle <110422010+DevlTz@users.noreply.github.com>
2025-09-25 11:07:41 -0300

    Delete run_example.sh

--- run_example.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-# Compile and run the example
-g++ -std=c++17 src/main.cpp src/expr_tree.cpp -o calc
-echo "Compilado com sucesso."
-echo "Executando: ./calc 1 2 + 3 *"
-./calc 1 2 + 3 *
